f99232799a570a966e176acb9ba304790c9dbe15
eric <eric@89206296-533a-43d1-afae-6104192af348>
2010-08-21 20:32:02 +0000

    Fixed GPL message
    git-svn-id: file:///home/eric/deprecated/svndir/trunk@11 89206296-533a-43d1-afae-6104192af348

--- content-script.js
+++ content-script.js
@@ -11,7 +11,7 @@
  * for the specific language governing rights and limitations under the
  * License.
  *
- * The Original Code is Password Hasher Redux
+ * The Original Code is Password Hasher Plus
  *
  * The Initial Developer of the Original Code is Eric Woodruff.
  * Portions created by the Initial Developer are Copyright (C) 2010
