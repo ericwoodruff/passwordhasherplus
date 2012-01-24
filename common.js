d9019eaa2589a8e7a9586b737cf484a9dd42e1a4
Eric Woodruff <eric.woodruff@gmail.com>
2012-01-24 07:49:42 -0800

    renamed generate_guid to generateGuid

--- common.js
+++ common.js
@@ -44,7 +44,7 @@ String.prototype.substringAfter = function (str) {
 var default_length = 8;
 var default_strength = 2;
 
-function generate_guid () {
+function generateGuid () {
 	return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace (/[xy]/g, function(c) {
 		var r = Math.random ()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
 		return v.toString (16);
