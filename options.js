23d939ab735f57ba196677e24329bf4209d972e1
Jeoffrey Bakker <jeoffrey@finalist.nl>
2014-01-23 10:50:13 -0800

    remove inline event onclick/onload handlers
    Finish merge of pull request from from Carl Baldwin <carl@ecbaldwin.net> and Jeoffrey Bakker <jeoffrey@finalist.nl>

--- options.js
+++ options.js
@@ -77,3 +77,22 @@ function loadStorage () {
 	restoreOptions ();
 	refreshStorage ();
 }
+
+// Add event listeners once the DOM has fully loaded by listening for the
+// `DOMContentLoaded` event on the document, and adding your listeners to
+// specific elements when it triggers.
+document.addEventListener('DOMContentLoaded', function () {
+    restoreOptions ();
+    refreshStorage ();
+
+    $('#generate').click(setNewGuid);
+    $('#backupSave').click(saveOptions);
+    $('#backupRevert').click(restoreOptions);
+    $('#removeUnUsedTags').click(function() {localStorage.collectGarbage (); refreshStorage ();});
+    $('#dbClear').click(function() {clearStorage (); refreshStorage ();});
+    $('#dbSave').click(loadStorage);
+    $('#dbRevert').click(refreshStorage);
+
+    $('#portablePage').click(function() {chrome.tabs.create({url:'chrome-extension://'+location.hostname+'/passhashplus.html'})});
+
+});
--- popup.js
+++ popup.js
@@ -47,3 +47,9 @@ $('#bump').click (function () {
 $('#tag').change (writeModel);
 $('#length').change (writeModel);
 $('#strength').change (writeModel);
+
+$(document).ready(function() {
+    $('#link-options').click(function() {
+        chrome.tabs.create({url:'chrome-extension://'+location.hostname+'/options.html'})
+    });
+})
