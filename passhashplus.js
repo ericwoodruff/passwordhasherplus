473ab5d6e71d773b132cdaf6cf07e22f6e864011
William Kenyon <git@wkenyon.net>
2014-06-16 01:25:48 +0000

    Reimplementation of passhashplus-data.js in passhashplus.js
    This code is well commented.
    read the comments.

--- passhashplus.js
+++ passhashplus.js
@@ -81,14 +81,106 @@ $( document ).ready(function () {
       $('#databasetextarea').css('display','none');
     }
   });
+
+  if (JSON.parse( $('#is-file-portable').text() )){
+
+     //////////////////////////////////////////////
+     // File is portable:                        //
+     //                                          //
+     //   * Scripts are already inlined          //
+     //   * Database already populated           //
+     //   * Download button already invisible... //
+     //                                          //
+     // Nothing to do.                           //
+     //////////////////////////////////////////////
+     invariant_startup_code();
+  } else {
+
+     /////////////////////////////////////////////////////////////////////////////
+     // File is not portable:                                                   //
+     //                                                                         //
+     // 1. Inline all javascript scripts                                        //
+     //   1.a. Grab the contents of wherever src= points to.                    //
+     //   1.b. Put those contents in between <script> tags                      //
+     //   1.c. Remove the old "src=" attribute                                  //
+     //   1.d. Leave a comment to say what the old "src=" attribute was         //
+     //                                                                         //
+     // 2. Populate the <script id="database">                                  //
+     //                                                                         //
+     // 3.When the ajax from //1 is all finished: Set up the Download button    //
+     //   3.a. Encode the entire DOM as a URI, and set it to the href of the    //
+     //        download button                                                  //
+     //   3.b. Make the download button visible.                                //
+     //        Note that the download button is made visible _after_ the DOM -> //
+     //        URI encoding. Therefore, it will be invisible in the downloaded  //
+     //        file                                                             //
+     /////////////////////////////////////////////////////////////////////////////
+
+
+    $('#is-file-portable').text("true");
+    var gettings = $("script").map(function(_i, script){ //1
+      if (script.type == "text/javascript"){
+        return $.get($(script).attr('src'), function (data) { //1.a
+          $(script).text (data); //1.b
+          $(script).before('<!-- src="' + $(script).attr('src') + '" -->'); //1.d
+          $(script).removeAttr("src"); //1.c
+        });
+      } //if (script.type == "text/javascript")
+    });
+
+    $('#database').text(dumpDatabase()); //2
+
+    $.when.apply(null, gettings).done(function(){ //3
+      var uriContent = "data:application/octet-stream,"
+                       + encodeURIComponent($('html').html());
+      $('#save').attr('href', uriContent); //3.a
+      $('#savediv').css('display','block'); //3.b
+      invariant_startup_code();
+    });
+  }
 }); //end of $(document).ready()
 
-function loadJsonData () {
-	options=JSON.parse($('#json-internal-options').text ());
-	$("script[id^=json-config]").each (function () {
-		var url = $(this).attr("id").replace (/json-config-/, "");
-		urls[url] = JSON.parse($(this).text ());
-	});
+
+function invariant_startup_code(){
+  //invariant_startup_code can be entered from two places,
+  //immediately at startup for
+  //////////////////////////////////////////////////////////////////////////
+  // This code is run regardless of wheather the file is portable or not. //
+  //                                                                      //
+  // 4. Parse the database, and put it in a global for everyone to use    //
+  //                                                                      //
+  // 5. Populate drop down list from database                             //
+  //   5.b. for each site tag in the database                             //
+  //      5.b.i add it as an option to the drop down list                 //
+  //                                                                      //
+  // 6. Populate database textarea from database                          //
+  //                                                                      //
+  // 7. Pretend the drop down list just got changed, so that the text     //
+  //    fields get populated                                              //
+  //                                                                      //
+  //////////////////////////////////////////////////////////////////////////
+
+
+  //4 set global variable [[database]]
+  database=JSON.parse($('#database').text ()); //4.a.
+
+  //5
+  var regexp = /tag:(.+)/;
+  for (var key in database) {
+    if (key.match(regexp)){ //5.b.
+      var sitetag = regexp.exec(key)[1];
+      $('#urls').append ($("<option></option>").text(sitetag)); //5.b.i
+    }
+  }
+
+  $('#databasetextarea').text($('#database').text()); //6
+
+  $('.nopasshash').on("keyup", update);     //x
+  $('.nopasshash').on("change", update);    //y
+  $('#urls').on("change", selectionChanged);//z
+
+  //x,y,z are here to make sure database is defined before
+  //update/selectionChaned can fire
+
+  selectionChanged(); //7
 }
-  $('.nopasshash').on("keyup", update);         //x
-  $('.nopasshash').on("change", update);          //y
